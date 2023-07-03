36062fd0833b3d1f998aa746c34ff95900c17923
Charles Joshua Houser <cjohouser@gmail.com>
2023-07-03 03:48:26 +0000

    premature optimization

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -6,66 +6,69 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/cjhouser/washere/models"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type signaturePageData struct {
-	PageTitle  string
-	Signatures []string
+type server struct {
+	readerConnection *grpc.ClientConn
+	template         *template.Template
 }
 
-func main() {
-	http.HandleFunc("/", serveHome)
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
+func (s server) home(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		conn, err := grpc.Dial("reader:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer conn.Close()
-		client := models.NewSignatureClient(conn)
-		signatureRequest := &models.SignatureRequest{}
+		client := models.NewSignatureClient(s.readerConnection)
 		signatures := []string{}
-		stream, err := client.Get(context.Background(), signatureRequest)
+		stream, err := client.Get(context.Background(), &models.SignatureRequest{})
 		if err != nil {
-			log.Println(err)
+			log.Println("ERROR:", "failed to initialize message stream", err)
 			return
 		}
-		for {
-			signatureResponse, err := stream.Recv()
-			if err == io.EOF {
-				break
-			}
+		for signatureResponse, err := stream.Recv(); err != io.EOF; {
 			if err != nil {
-				log.Println(err)
+				log.Println("ERROR:", "error receiving from message stream", err)
 				return
+			} else {
+				signatures = append(signatures, signatureResponse.GetSignature())
 			}
-			signatures = append(signatures, signatureResponse.GetSignature())
 		}
-		template := template.Must(template.ParseFiles("index.html"))
-		data := signaturePageData{
+		data := struct {
+			PageTitle  string
+			Signatures []string
+		}{
 			PageTitle:  "washere",
 			Signatures: signatures,
 		}
-		err = template.Execute(w, data)
-		if err != nil {
-			log.Println(err)
+		if s.template.Execute(w, data) != nil {
+			log.Println("ERROR:", "template generation error", err)
+			return
 		}
-		log.Println(http.StatusOK, "GET", "/")
+		log.Println("INFO:", r.Method, r.RequestURI, http.StatusOK)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(`{"message": "not found"}`))
-		log.Println(http.StatusNotFound)
+		log.Println("ERROR:", r.Method, r.RequestURI, http.StatusNotFound)
 	}
+}
+
+func main() {
+	listenSocket := os.Getenv("LISTEN_SOCKET")
+	readerSocket := os.Getenv("READER_SOCKET")
+	// Open a reusable connection to the reader service
+	readerConnection, err := grpc.Dial(readerSocket, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Println("ERROR:", "grpc dailing error", err)
+		return
+	}
+	defer readerConnection.Close()
+	server := server{
+		readerConnection,
+		template.Must(template.ParseFiles("index.html")),
+	}
+	http.HandleFunc("/", server.home)
+	log.Println("INFO: listening on", listenSocket)
+	http.ListenAndServe(listenSocket, nil)
 }
